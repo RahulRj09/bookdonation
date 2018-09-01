20275eb9306c859c43d5331936402525a068579d
RahulRj09 <rahul18@navgurukul.org>
2018-09-01 19:40:40 +0530

    Added new changes

--- bookdonation/bookdonation/settings.py
+++ bookdonation/bookdonation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*x-euyup4!@ji#)la1co3i9r9#p$=9l-g-@*jwhsdzv-1gpj1#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','pythonanywhere.com']
 
 
 # Application definition
